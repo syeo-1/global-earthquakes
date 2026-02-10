ecb23c2f739173124253865df0eb6b259d7a68a3
syeo-1 <seansqyeo@gmail.com>
2026-02-10 13:41:56 -0800

    use pipe as delimiter and add time and place information

--- coordinate_gathering.py
+++ coordinate_gathering.py
@@ -12,7 +12,7 @@ for the past 30 days
 
 All earthqquakes REST endpoint for last 30 days: https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson
 '''
-import json, requests
+import json, requests, sys
 
 from plotly.graph_objs import Scattergeo, Layout
 from plotly import offline
@@ -57,6 +57,7 @@ def process_earthquake_data(earthquake_data):
         {
             magnitude: 4.5,
             time_occured: 1770071929530,
+            place: '7 km NNW of Meadow Lakes, Alaska
             lattitude: 103.12
             longitude: 24.32
         },
@@ -71,6 +72,7 @@ def process_earthquake_data(earthquake_data):
         processed_feature_data = {
             'magnitude': feature_data['properties']['mag'],
             'time_occured': feature_data['properties']['time'],
+            'place': feature_data['properties']['place'],
             'longitude': feature_data['geometry']['coordinates'][0],
             'lattitude': feature_data['geometry']['coordinates'][1]
         }
@@ -100,6 +102,34 @@ def process_earthquake_data_plotly(processed_earthquake_data):
         'lattitudes': lattitudes
     }
 
+def process_earthquake_data_leaflet(processed_earthquake_data):
+    '''
+    process earthquake data for the data in order to be displayed
+    for the leaflet map
+    '''
+    magnitudes = []
+    time_occured = []
+    places = []
+    longitudes = []
+    lattitudes = []
+
+    for feature_data in processed_earthquake_data:
+        # print(feature_data.keys())
+        magnitudes.append(feature_data['magnitude'])
+        time_occured.append(feature_data['time_occured'])
+        places.append(feature_data['place'])
+        longitudes.append(feature_data['longitude'])
+        lattitudes.append(feature_data['lattitude'])
+
+
+    return {
+        'magnitudes': magnitudes,
+        'times_occured': time_occured,
+        'places': places,
+        'longitudes': longitudes,
+        'lattitudes': lattitudes
+    }
+
 def graph_earthquake_data(earthquake_data):
     '''
     graph out the earthquake data onto a plotly map
@@ -113,21 +143,37 @@ def graph_earthquake_data(earthquake_data):
     }
     offline.plot(figure, filename='global_earthquakes.html')
 
+def output_data_for_leaflet(leaflet_data):
+    sys.stdout.buffer.write('magnitude|longitude|lattitude|time|place\n'.encode('utf-8'))
+
+    for data in zip(
+        leaflet_data['magnitudes'],
+        leaflet_data['longitudes'],
+        leaflet_data['lattitudes'],
+        leaflet_data['times_occured'],
+        leaflet_data['places'],
+    ):
+        sys.stdout.buffer.write(f'{data[0]}|{data[1]}|{data[2]}|{data[3]}|{data[4]}\n'.encode('utf-8'))
+
+
 
 def main():
     earthquake_feature_data = retrieve_earthquake_feature_data_from_api()
     processed_earthquake_data = process_earthquake_data(earthquake_feature_data)
+    leaflet_processed_earthquake_data = process_earthquake_data_leaflet(processed_earthquake_data)
+    output_data_for_leaflet(leaflet_processed_earthquake_data)
+
     # print(processed_earthquake_data)
-    plotly_graph_data = process_earthquake_data_plotly(processed_earthquake_data)
+    # plotly_graph_data = process_earthquake_data_plotly(processed_earthquake_data)
     
     # adhoc data printout for csv
-    print('magnitude,longitude,lattitude')
-    for data in zip(
-        plotly_graph_data['magnitudes'],
-        plotly_graph_data['longitudes'],
-        plotly_graph_data['lattitudes'],
-    ):
-        print(f'{data[0]},{data[1]},{data[2]}')
+    # print('magnitude,longitude,lattitude')
+    # for data in zip(
+    #     plotly_graph_data['magnitudes'],
+    #     plotly_graph_data['longitudes'],
+    #     plotly_graph_data['lattitudes'],
+    # ):
+    #     print(f'{data[0]},{data[1]},{data[2]}')
 
     # graph_earthquake_data(plotly_graph_data)
 
